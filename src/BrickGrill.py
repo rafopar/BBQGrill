8738fe55e67c235dfdc0a5de1ba7420b31262278
Rafayel Paremuzyan <rafopar@new-host-5.home>
2024-08-03 13:36:09 -0400

    Still Work in progress * On side walls a concrete gap is added between neighboring bricks * Concrete base is added * Two pictures are added representing the current state * The FCStd file (this is how the FREECAD saves) is added of the current stats

--- src/BrickGrill.py
+++ src/BrickGrill.py
@@ -64,15 +64,19 @@ std_brick_h = 57.15  # height of the standard brick
 half_brick_l = std_brick_l*0.5 # length of the standard brick
 
 
-concr_width = 4  # The thickness of the concrete
-n_width = 6
-n_back_wall = 14
+concr_width = 4       # The thickness of the concrete
+n_width = 6           # Number of full bricks on side walls in a given layer
+n_back_wall = 14      # Number of full bricks on the back wall in a given layer
+n_lvl1_layers = 15    # Number of Layers in level 1
 
+base_Width = (n_width+0.5)*(std_brick_l + concr_width)
+base_Length = (n_back_wall+2.5)*(std_brick_l + concr_width)
+base_Height = 300    # mm
 
-#obj = cubo("test_cube", std_brick_h, std_brick_w, std_brick_l)
+baseConcrete = cubo("baseConcrete" , base_Height, base_Length, base_Width)
+baseConcrete.Placement = FreeCAD.Placement(Vector(- base_Height, 0, 0 ), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 
-
-for i_h in range(0, 13):
+for i_h in range(0, n_lvl1_layers):
 
 #
 #   Building the left wall
@@ -91,7 +95,7 @@ for i_h in range(0, 13):
 			std_brick_1 = cubo("brick_%d_1" %(i), std_brick_h, std_brick_w, std_brick_l)
 			std_brick_1.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), 0, i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 			std_brick_2 = cubo("brick_%d_2" %(i), std_brick_h, std_brick_w, std_brick_l)
-			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w , i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + concr_width , i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 		std_brick_edge = cubo("brick_%d_2" %(n_width), std_brick_h, std_brick_w, std_brick_l)
 		std_brick_edge.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_l, n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
 	else:
@@ -104,10 +108,10 @@ for i_h in range(0, 13):
 
 			if i < n_width - 1:
 				std_brick_2 = cubo("brick_%d_2" %(i), std_brick_h, std_brick_w, std_brick_l)
-				std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w , i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+				std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + concr_width, i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 			else:
 				half_brick = cubo("brick_back_%d_%d" %(i_h, n_back_wall+1), std_brick_h, std_brick_w, half_brick_l)
-				half_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w , i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+				half_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + concr_width, i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 
 	#
 	#   Building the Back wall
@@ -117,11 +121,8 @@ for i_h in range(0, 13):
 		for i_bw in range(0, n_back_wall+1):
 			std_brick = cubo("brick_back_%d_%d" %(i_h, i_bw), std_brick_h, std_brick_w, std_brick_l)
 			std_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), 0.5*std_brick_l + (i_bw+1)*(std_brick_l+concr_width), n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
-		#half_brick = cubo("brick_back_%d_%d" %(i_h, n_back_wall+1), std_brick_h, std_brick_w, half_brick_l)
-#		half_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_l + (n_back_wall)*(std_brick_l+concr_width) + half_brick_l, n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
-	else:
-#		half_brick = cubo("brick_back_%d_%d" %(i_h, 0), std_brick_h, std_brick_w, half_brick_l)
-#		half_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_l + half_brick_l+concr_width, n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
+	
+	else:	
 		for i_bw in range(0, n_back_wall + 1):
 			std_brick = cubo("brick_back_%d_%d" %(i_h, i_bw+1), std_brick_h, std_brick_w, std_brick_l)
 			std_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_l + (i_bw+1)*(std_brick_l+concr_width), n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
@@ -136,7 +137,7 @@ for i_h in range(0, 13):
 			std_brick_1 = cubo("brick_Right_%d_1" %(i), std_brick_h, std_brick_w, std_brick_l)
 			std_brick_1.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l, i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 			std_brick_2 = cubo("brick_Right_%d_2" %(i), std_brick_h, std_brick_w, std_brick_l)
-			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l , i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + concr_width + (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l , i*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 
 		std_brick = cubo("brick_back_%d_%d" %(i_h, n_back_wall+1), std_brick_h, std_brick_w, std_brick_l)
 		std_brick.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_l + (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l, n_width*(std_brick_l+concr_width)), FreeCAD.Rotation(0, 0, 90), Vector(0, 0, 0))
@@ -146,7 +147,7 @@ for i_h in range(0, 13):
 
 		for i in range(0, n_width ):
 			std_brick_2 = cubo("brick_Right_%d_2" %(i), std_brick_h, std_brick_w, std_brick_l)
-			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l, i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
+			std_brick_2.Placement = FreeCAD.Placement(Vector(i_h*(std_brick_h + concr_width), std_brick_w + concr_width + (n_back_wall+1)*(std_brick_l+concr_width) + half_brick_l, i*(std_brick_l+concr_width) + std_brick_w), FreeCAD.Rotation(0, 0, 0), Vector(0, 0, 0))
 
 			if i < n_width - 1:
 				std_brick_1 = cubo("brick_Right_%d_1" %(i), std_brick_h, std_brick_w, std_brick_l)
